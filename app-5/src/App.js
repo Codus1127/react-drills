e5a901ec410aeee32fcf18487cee53cd9a907d9b
Cody Young <codyjamesyoung@gmail.com>
2019-08-29 09:49:22 -0600

    complete

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from "./Image";
+import Water from './water.png'
 
 class App extends Component {
+  constructor(){
+    super();
+    // this.state =
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image myImage={"https://http.cat/409"} />
+        <img src={Water} />
       </div>
+
     );
   }
 }
